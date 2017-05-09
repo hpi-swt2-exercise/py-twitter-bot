682062235b51af881c9d802263d2344a28b2fee9
Fireangel <hallo@web.de>
2017-05-09 19:20:10 +0200

    hallo

--- tweet_text.py
+++ tweet_text.py
@@ -4,6 +4,7 @@
 from datetime import datetime
 # convert times according to time zones
 from pytz import timezone
+from requests import get
 
 def reply(tweet):
     """Return text to be used as a reply"""
@@ -16,8 +17,9 @@ def reply(tweet):
     return None
 
 def idle_text():
-    """Return text that is tweeted when not replying"""
-    # Construct the text we want to tweet out (140 chars max)
-    berlin_time = datetime.now(timezone('Europe/Berlin'))
-    text = berlin_time.strftime("It is %H:%M:%S on a %A (%d-%m-%Y).")
-    return text
+    # Some more ideas: https://www.programmableweb.com/category/humor/api
+    data = get('https://api.chucknorris.io/jokes/random').json()
+    joke = data['value']
+    return joke[0:139]
+
+
